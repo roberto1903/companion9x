6c860c3fcd903023758890eb5b9e67d77115fe05
bsongis <bsongis@6b3858b5-4ca4-83a9-f672-1595cfb9b461>
2011-12-27 09:43:12 +0000

    Sticks indexes were badly converted in applyTemplate function

--- src/helpers.h
+++ src/helpers.h
@@ -9,12 +9,7 @@
 //NOTICE!  =>  1..4 -> 1..4
 #define CONVERT_MODE(x) (((x)<=4) ? modn12x3[g_eeGeneral.stickMode][((x)-1)] : (x))
 #define CHANNEL_ORDER(x) (chout_ar[g_eeGeneral.templateSetup*4 + (x)-1])
-#define THR_STICK       (2-(g_eeGeneral.stickMode&1))
-#define ELE_STICK       (1+(g_eeGeneral.stickMode&1))
-#define AIL_STICK       ((g_eeGeneral.stickMode&2) ? 0 : 3)
-#define RUD_STICK       ((g_eeGeneral.stickMode&2) ? 3 : 0)
 
-#define CM(x) (CONVERT_MODE(x))  //good for SRC
 #define CH(x) (CHOUT_BASE+(x))
 #define CV(x) (CURVE_BASE+(x)-1)
 #define CC(x) (CHANNEL_ORDER(x)) //need to invert this to work with dest
