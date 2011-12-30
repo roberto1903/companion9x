caa95ff4b40434f0533404a269a9df0b7da8470b
romolo.manfredini <romolo.manfredini@gmail.com>
2011-12-30 18:41:00 +0000

    Translatable contributor dialog

--- src/contributorsdialog.cpp
+++ src/contributorsdialog.cpp
@@ -1,19 +1,39 @@
 #include "contributorsdialog.h"
 #include "ui_contributorsdialog.h"
 #include <QtGui>
+#define CLINESEP "=====================================================\n"
 
 contributorsDialog::contributorsDialog(QWidget *parent) :
     QDialog(parent, Qt::WindowTitleHint | Qt::WindowSystemMenuHint),
     ui(new Ui::contributorsDialog)
 {
     ui->setupUi(this);
-
+    ui->plainTextEdit->insertPlainText(CLINESEP);
+    ui->plainTextEdit->insertPlainText(tr("People who have contributed to this project")+"\n");
+    ui->plainTextEdit->insertPlainText(CLINESEP);
+    
     QFile file(":/contributors");
     if(file.open( QIODevice::ReadOnly | QIODevice::Text ) )
     {
         ui->plainTextEdit->insertPlainText(file.readAll());
     }
-
+    
+    ui->plainTextEdit->insertPlainText("\n");
+    ui->plainTextEdit->insertPlainText(CLINESEP);
+    ui->plainTextEdit->insertPlainText(tr("Coders")+"\n");
+    ui->plainTextEdit->insertPlainText(CLINESEP);
+    
+    QFile file2(":/coders");
+    if(file2.open( QIODevice::ReadOnly | QIODevice::Text ) )
+    {
+        ui->plainTextEdit->insertPlainText(file2.readAll());
+    }
+    
+    ui->plainTextEdit->insertPlainText("\n\n\n");
+    ui->plainTextEdit->insertPlainText(tr("Honors go to Rafal Tomczak (RadioClone) and Thomas Husterer (th9x) \nof course. Also to Erez Raviv (er9x) and it's fantastic eePe, from which\ncompanion9x was forked out."));
+    ui->plainTextEdit->insertPlainText("\n\n");
+    ui->plainTextEdit->insertPlainText(tr("Thank you all !!!"));
+    
     ui->plainTextEdit->setReadOnly(true);
     ui->plainTextEdit->centerOnScroll();
     ui->plainTextEdit->verticalScrollBar()->setValue(0);
