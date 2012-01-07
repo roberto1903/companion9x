474445472c227f38cc61df61991452f224b40956
romolo.manfredini@gmail.com <romolo.manfredini@gmail.com@6b3858b5-4ca4-83a9-f672-1595cfb9b461>
2012-01-07 12:47:15 +0000

    Changes in eeprominterface

--- trunk/src/flashinterface.cpp
+++ trunk/src/flashinterface.cpp
@@ -15,218 +15,209 @@
  */
 #include <QtGui>
 #include "hexinterface.h"
+#include "splash.h"
 #include "flashinterface.h"
 
 FlashInterface::FlashInterface(QString fileName) {
-        version="";
-        date="";
-        time="";
-        svn="";
-        build="";
-        
-        QFile file(fileName);
+  uint8_t temp[MAX_FSIZE];
+  version = "";
+  date = "";
+  time = "";
+  svn = "";
+  build = "";
 
-        if (!file.open(QIODevice::ReadOnly | QIODevice::Text)) {  //reading HEX TEXT file
-            isValid=false;
-        } else {
-            QTextStream inputStream(&file);
-            flash_size = HexInterface(inputStream).load(flash, MAX_FSIZE);
-            file.close();
-            inputStream.reset();
-            if (flash_size==0) {
-                QFile file(fileName);
-                file.open(QIODevice::ReadOnly);
-                char bin_flash[MAX_FSIZE];
-                flash_size=file.read(bin_flash,MAX_FSIZE);
-                for (int i=0; i<flash_size;i++)
-                    flash[i]=(uint8_t)bin_flash[i];
-            }
-            if (flash_size>0) {
-                SeekVer();
-                SeekSvn();
-                SeekDate();
-                SeekTime();
-                SeekBuild();
-            }
-        }
+  QFile file(fileName);
+
+  if (!file.open(QIODevice::ReadOnly | QIODevice::Text)) { //reading HEX TEXT file
+    isValid = false;
+  }
+  else {
+    QTextStream inputStream(&file);
+    int flash_size = HexInterface(inputStream).load(temp, MAX_FSIZE);
+    file.close();
+    inputStream.reset();
+    if (flash_size == 0) {
+      QFile file(fileName);
+      file.open(QIODevice::ReadOnly);
+      char bin_flash[MAX_FSIZE];
+      flash_size = file.read(bin_flash, MAX_FSIZE);
+      flash = QByteArray::fromRawData(bin_flash, flash_size);
+    }
+    else {
+      flash = QByteArray::fromRawData((const char *) temp, flash_size);
+    }
+    if (flash_size > 0) {
+      SeekVer();
+      SeekSvn();
+      SeekDate();
+      SeekTime();
+      SeekBuild();
+      SeekSplash();
+    }
+  }
 }
 
 QString FlashInterface::getVers(void) {
-    return version;
+  return version;
 }
 
 QString FlashInterface::getDate(void) {
-    return date;
+  return date;
 }
 
 QString FlashInterface::getTime(void) {
-    return time;
+  return time;
 }
 
 QString FlashInterface::getSvn(void) {
-    return svn;
+  return svn;
 }
 
 QString FlashInterface::getBuild(void) {
-    return build;
+  return build;
 }
 
 void FlashInterface::SeekVer(void) {
-    int i, start=-1, end=-1;
-    char tmp[20];
-    for( int i=0; i<flash_size-5; i++) {
-        if (flash[i]=='V' && flash[i+1]=='E' && flash[i+2]=='R' && flash[i+3]=='S' && flash[i+4]==':') {
-            start=i;
-            break;
-        }
+  int i, start = -1, end = -1;
+  start = flash.indexOf(QString(VERS_MARK));
+  if (start > 0) {
+    start += QString(VERS_MARK).length();
+    for (i = start; i < (start + 20); i++) {
+      if (flash.at(i) == 0) {
+        end = i;
+        break;
+      }
     }
-    if (start>0 ) {
-        for (i=start; i<(start+20); i++) {
-            if (flash[i]==0) {
-                end=i;
-                break;
-            }
-        }
-        if (end>0) {
-            for (i=(start+5); i<(end+1);i++) {
-                tmp[i-(start+5)]=flash[i];
-            }
-            version=QString(tmp).trimmed();
-        } else {
-            version=QString("");
-        }
+    if (end > 0) {
+      version = QString(flash.mid(start, (end - start))).trimmed();
     }
+    else {
+      version = QString("");
+    }
+  }
 }
 
 void FlashInterface::SeekSvn(void) {
-    int i, start=-1, end=-1;
-    char tmp[20];
-    for( int i=0; i<flash_size-4; i++) {
-        if (flash[i]=='S' && flash[i+1]=='V' && flash[i+2]=='N' && flash[i+3]==':' ) {
-            start=i;
-            break;
-        }
+  int i, start = -1, end = -1;
+  start = flash.indexOf(QString(SVN_MARK));
+  if (start > 0) {
+    start += QString(SVN_MARK).length();
+    for (i = start; i < (start + 20); i++) {
+      if (flash.at(i) == 0) {
+        end = i;
+        break;
+      }
     }
-    if (start>0 ) {
-        for (i=start; i<(start+20); i++) {
-            if (flash[i]==0) {
-                end=i;
-                break;
-            }
-        }
-        if (end>0) {
-            for (i=(start+4); i<(end+1);i++) {
-                tmp[i-(start+3)]=flash[i];
-            }
-            svn=QString(tmp).trimmed();
-        } else {
-            svn=QString("");
-        }
+    if (end > 0) {
+      svn = QString(flash.mid(start, (end - start))).trimmed();
     }
+    else {
+      svn = QString("");
+    }
+  }
 }
 
 void FlashInterface::SeekDate(void) {
-    int i, start=-1, end=-1;
-    char tmp[20];
-    for( int i=0; i<flash_size-5; i++) {
-        if (flash[i]=='D' && flash[i+1]=='A' && flash[i+2]=='T' && flash[i+3]=='E' && flash[i+4]==':') {
-            start=i;
-            break;
-        }
+  int i, start = -1, end = -1;
+  start = flash.indexOf(QString(DATE_MARK));
+  if (start > 0) {
+    start += QString(DATE_MARK).length();
+    for (i = start; i < (start + 20); i++) {
+      if (flash.at(i) == 0) {
+        end = i;
+        break;
+      }
     }
-    if (start>0 ) {
-        for (i=start; i<(start+20); i++) {
-            if (flash[i]==0) {
-                end=i;
-                break;
-            }
-        }
-        if (end>0) {
-            for (i=(start+5); i<(end+1);i++) {
-                tmp[i-(start+5)]=flash[i];
-            }
-            date=QString(tmp).trimmed();
-        } else {
-            date=QString("");
-        }
+    if (end > 0) {
+      date = QString(flash.mid(start, (end - start))).trimmed();
     }
+    else {
+      date = QString("");
+    }
+  }
 }
 
 void FlashInterface::SeekTime(void) {
-    int i, start=-1, end=-1;
-    char tmp[20];
-    for( int i=0; i<flash_size-5; i++) {
-        if (flash[i]=='T' && flash[i+1]=='I' && flash[i+2]=='M' && flash[i+3]=='E' && flash[i+4]==':') {
-            start=i;
-            break;
-        }
+  int i, start = -1, end = -1;
+  start = flash.indexOf(QString(TIME_MARK));
+  if (start > 0) {
+    start += QString(TIME_MARK).length();
+    for (i = start; i < (start + 20); i++) {
+      if (flash.at(i) == 0) {
+        end = i;
+        break;
+      }
     }
-    if (start>0 ) {
-        for (i=start; i<(start+20); i++) {
-            if (flash[i]==0) {
-                end=i;
-                break;
-            }
-        }
-        if (end>0) {
-            for (i=(start+5); i<(end+1);i++) {
-                tmp[i-(start+5)]=flash[i];
-            }
-            time=QString(tmp).trimmed();
-        } else {
-            time=QString("");
-        }
+    if (end > 0) {
+      time = QString(flash.mid(start, (end - start))).trimmed();
     }
+    else {
+      time = QString("");
+    }
+  }
 }
 
 void FlashInterface::SeekBuild(void) {
-    int i;
-    int start=-1;
-    int end=-1;
-    char tmp[20]; 
-    for( int i=0; i<flash_size-5; i++) {
-        if (flash[i]=='B' && flash[i+1]=='L' && flash[i+2]=='D' && flash[i+3]==':' ) {
-            start=i;
-            break;
-        }
+  int i, start = -1, end = -1;
+  start = flash.indexOf(QString(BLD_MARK));
+  if (start > 0) {
+    start += QString(BLD_MARK).length();
+    for (i = start; i < (start + 20); i++) {
+      if (flash.at(i) == 0) {
+        end = i;
+        break;
+      }
     }
-    if (start>0 ) {
-        for (i=start; i<(start+20); i++) {
-            if (flash[i]==0) {
-                end=i;
-                break;
-            }
-        }
-        if (end>0) {
-            for (i=(start+5); i<(end+1);i++) {
-                tmp[i-(start+5)]=flash[i];
-            }
-            build=QString(tmp).trimmed();
-        } else {
-            build=QString("");
-        }
-    } else {
-        for( i=0; i<flash_size-5; i++) {
-            if (flash[i]=='M' && flash[i+1]=='O' && flash[i+2]=='D' && flash[i+3]==':' ) {
-                start=i;
-                break;
-            }
-        }
-        if (start>0 ) {
-            for (i=start; i<(start+20); i++) {
-                if (flash[i]==0) {
-                    end=i;
-                    break;
-                }
-            }
-            if (end>0) {
-                for (i=(start+5); i<(end+1);i++) {
-                    tmp[i-(start+5)]=flash[i];
-                }
-                build=QString(tmp).trimmed();
-            } else {
-                build=QString("");
-            }
-        }
+    if (end > 0) {
+      build = QString(flash.mid(start, (end - start))).trimmed();
     }
+    else {
+      build = QString("");
+    }
+  }
+  else {
+    start = flash.indexOf(QString(MOD_MARK));
+    if (start > 0) {
+      start += QString(MOD_MARK).length();
+      for (i = start; i < (start + 20); i++) {
+        if (flash.at(i) == 0) {
+          end = i;
+          break;
+        }
+      }
+      if (end > 0) {
+        build = QString(flash.mid(start, (end - start))).trimmed();
+      }
+      else {
+        build = QString("");
+      }
+    }
+  }
+}
+
+void FlashInterface::SeekSplash(void) {
+  QByteArray splash;
+  splash_offset=0;
+  splash.clear();
+  splash.append(gr9x_splash, sizeof(gr9x_splash));
+  int start = flash.indexOf(splash);
+  if (start>0) {
+    splash_offset=start;
+  } 
+  if (start==-1) {
+    splash.clear();
+    splash.append(er9x_splash, sizeof(er9x_splash));
+    start = flash.indexOf(splash);
+    if (start>0) {
+      splash_offset=start;
+    }
+  }
+  if (start==-1) {
+    splash.clear();
+    splash.append(gr9xv4_splash, sizeof(gr9xv4_splash));
+    start = flash.indexOf(splash);
+    if (start>0) {
+      splash_offset=start;
+    }
+  }
 }
