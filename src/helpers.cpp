0c9763de97e200b92a8d18235435e5eb9d0069c0
romolo.manfredini <romolo.manfredini@gmail.com>
2012-01-10 00:44:22 +0000

    Store custom image in settings - preliminary work for FW autopatch

--- src/helpers.cpp
+++ src/helpers.cpp
@@ -4,117 +4,103 @@
 
 #define SWITCHES_STR "THR""RUD""ELE""ID0""ID1""ID2""AIL""GEA""TRN""SW1""SW2""SW3""SW4""SW5""SW6""SW7""SW8""SW9""SWA""SWB""SWC"
 
-QString getPhaseName(int val)
-{
-    if(!val) return "---";
-    return QString(val<0 ? "!" : "") + QString("FP%1").arg(abs(val)-1);
+QString getPhaseName(int val) {
+  if (!val) return "---";
+  return QString(val < 0 ? "!" : "") + QString("FP%1").arg(abs(val) - 1);
 }
 
-QString getSWName(int val)
-{
+QString getSWName(int val) {
 
-    if(!val) return "---";
-    if(val==MAX_DRSWITCH) return "ON";
-    if(val==-MAX_DRSWITCH) return "OFF";
+  if (!val) return "---";
+  if (val == MAX_DRSWITCH) return "ON";
+  if (val == -MAX_DRSWITCH) return "OFF";
 
-    return QString(val<0 ? "!" : "") + QString(SWITCHES_STR).mid((abs(val)-1)*3,3);
+  return QString(val < 0 ? "!" : "") + QString(SWITCHES_STR).mid((abs(val) - 1)*3, 3);
 }
 
-void populateSwitchCB(QComboBox *b, int value)
-{
-    b->clear();
-    for(int i=-MAX_DRSWITCH; i<=MAX_DRSWITCH; i++)
-        b->addItem(getSWName(i));
-    b->setCurrentIndex(value+MAX_DRSWITCH);
-    b->setMaxVisibleItems(10);
+void populateSwitchCB(QComboBox *b, int value) {
+  b->clear();
+  for (int i = -MAX_DRSWITCH; i <= MAX_DRSWITCH; i++)
+    b->addItem(getSWName(i));
+  b->setCurrentIndex(value + MAX_DRSWITCH);
+  b->setMaxVisibleItems(10);
 }
 
 #define FSWITCH_STR  "----          ""Trainer       ""Trainer RUD   ""Trainer ELE   ""Trainer THR   ""Trainer AIL   ""Instant Trim  ""Trims2Offsets ""Telemetry View"
 #define FSW_LEN_FUNC 14
 
-QString getFuncName(unsigned int val)
-{
+QString getFuncName(unsigned int val) {
   return QString(FSWITCH_STR).mid(val*FSW_LEN_FUNC, FSW_LEN_FUNC);
 }
 
-void populateFuncCB(QComboBox *b, unsigned int value)
-{
-    b->clear();
-    for(unsigned int i=0; i<FuncCount; i++)
-      b->addItem(getFuncName(i));
-    b->setCurrentIndex(value);
-    b->setMaxVisibleItems(10);
+void populateFuncCB(QComboBox *b, unsigned int value) {
+  b->clear();
+  for (unsigned int i = 0; i < FuncCount; i++)
+    b->addItem(getFuncName(i));
+  b->setCurrentIndex(value);
+  b->setMaxVisibleItems(10);
 }
 
-
-void populatePhasesCB(QComboBox *b, int value)
-{
-    QString str = PHASES_STR;
-    b->clear();
-    for(int i=0; i<(str.length()/4); i++)  b->addItem(str.mid(i*4,4).replace("FP","Phase "));
-    b->setCurrentIndex(value+MAX_PHASES);
-    if (!GetEepromInterface()->getCapability(Phases))
-      b->setDisabled(true);
+void populatePhasesCB(QComboBox *b, int value) {
+  QString str = PHASES_STR;
+  b->clear();
+  for (int i = 0; i < (str.length() / 4); i++) b->addItem(str.mid(i * 4, 4).replace("FP", "Phase "));
+  b->setCurrentIndex(value + MAX_PHASES);
+  if (!GetEepromInterface()->getCapability(Phases))
+    b->setDisabled(true);
 }
 
-void populateCurvesCB(QComboBox *b, int value)
-{
-    b->clear();
-    for (int i=0; i<CURVE_BASE+MAX_CURVE5+MAX_CURVE9; i++)
-      b->addItem(getCurveStr(i));
-    b->setCurrentIndex(value);
-    b->setMaxVisibleItems(10);
+void populateCurvesCB(QComboBox *b, int value) {
+  b->clear();
+  for (int i = 0; i < CURVE_BASE + MAX_CURVE5 + MAX_CURVE9; i++)
+    b->addItem(getCurveStr(i));
+  b->setCurrentIndex(value);
+  b->setMaxVisibleItems(10);
 }
 
-void populateTrimUseCB(QComboBox *b, unsigned int phase)
-{
+void populateTrimUseCB(QComboBox *b, unsigned int phase) {
   b->addItem("Own trim");
-  for (unsigned int i=0; i<MAX_PHASES; i++) {
+  for (unsigned int i = 0; i < MAX_PHASES; i++) {
     if (i != phase) {
       b->addItem(QObject::tr("Flight phase %1 trim").arg(i));
     }
   }
 }
 
-void populateTimerSwitchCB(QComboBox *b, int value=0)
-{
-    b->clear();
-    for(int i=-TMR_NUM_OPTION; i<=TMR_NUM_OPTION; i++)
-        b->addItem(getTimerMode(i));
-    b->setCurrentIndex(value+TMR_NUM_OPTION);
-    b->setMaxVisibleItems(10);
+void populateTimerSwitchCB(QComboBox *b, int value = 0) {
+  b->clear();
+  for (int i = -TMR_NUM_OPTION; i <= TMR_NUM_OPTION; i++)
+    b->addItem(getTimerMode(i));
+  b->setCurrentIndex(value + TMR_NUM_OPTION);
+  b->setMaxVisibleItems(10);
 }
 
-QString getTimerMode(int tm)
-{
+QString getTimerMode(int tm) {
 
-    QString str = SWITCHES_STR;
-    QString stt = "OFFABSRUsRU%ELsEL%THsTH%ALsAL%P1 P1%P2 P2%P3 P3%";
+  QString str = SWITCHES_STR;
+  QString stt = "OFFABSRUsRU%ELsEL%THsTH%ALsAL%P1 P1%P2 P2%P3 P3%";
 
-    QString s;
-    if(abs(tm)<TMR_VAROFS)
-    {
-        s = stt.mid(abs(tm)*3,3);
-        if(tm<-1) s.prepend("!");
-        return s;
-    }
-
-    if(abs(tm)<(TMR_VAROFS+MAX_DRSWITCH-1))
-    {
-        s = str.mid((abs(tm)-TMR_VAROFS)*3,3);
-        if(tm<0) s.prepend("!");
-        return s;
-    }
-
-
-    s = "m" + str.mid((abs(tm)-(TMR_VAROFS+MAX_DRSWITCH-1))*3,3);
-    if(tm<0) s.prepend("!");
+  QString s;
+  if (abs(tm) < TMR_VAROFS) {
+    s = stt.mid(abs(tm)*3, 3);
+    if (tm<-1) s.prepend("!");
     return s;
+  }
+
+  if (abs(tm)<(TMR_VAROFS + MAX_DRSWITCH - 1)) {
+    s = str.mid((abs(tm) - TMR_VAROFS)*3, 3);
+    if (tm < 0) s.prepend("!");
+    return s;
+  }
+
+
+  s = "m" + str.mid((abs(tm)-(TMR_VAROFS + MAX_DRSWITCH - 1))*3, 3);
+  if (tm < 0) s.prepend("!");
+  return s;
 
 }
 
-QString getSourceStr(int idx)
-{
+QString getSourceStr(int idx) {
   switch (idx) {
     case 0:
       return QObject::tr("----");
@@ -195,49 +181,88 @@ QString getSourceStr(int idx)
   }
 }
 
-void populateSourceCB(QComboBox *b, int stickMode, int value)
-{
-    b->clear();
-    for(int i=0; i<37; i++) b->addItem(getSourceStr(i));
-    b->setCurrentIndex(value);
-    b->setMaxVisibleItems(10);
+void populateSourceCB(QComboBox *b, int stickMode, int value) {
+  b->clear();
+  for (int i = 0; i < 37; i++) b->addItem(getSourceStr(i));
+  b->setCurrentIndex(value);
+  b->setMaxVisibleItems(10);
 
-    for (int i=0; i<8-GetEepromInterface()->getCapability(ExtraChannels); i++) {
-      // Get the index of the value to disable
-      QModelIndex index = b->model()->index(SRC_CH16-i,0);
+  for (int i = 0; i < 8 - GetEepromInterface()->getCapability(ExtraChannels); i++) {
+    // Get the index of the value to disable
+    QModelIndex index = b->model()->index(SRC_CH16 - i, 0);
 
-      // This is the effective 'disable' flag
-      QVariant v(0);
+    // This is the effective 'disable' flag
+    QVariant v(0);
 
-      //the magic
-      b->model()->setData( index, v, Qt::UserRole -1);
-    }
+    //the magic
+    b->model()->setData(index, v, Qt::UserRole - 1);
+  }
 }
 
-
-QString getCSWFunc(int val)
-{
-    return QString(CSWITCH_STR).mid(val*CSW_LEN_FUNC,CSW_LEN_FUNC);
+QString getCSWFunc(int val) {
+  return QString(CSWITCH_STR).mid(val*CSW_LEN_FUNC, CSW_LEN_FUNC);
 }
 
-
-void populateCSWCB(QComboBox *b, int value)
-{
-    b->clear();
-    for(int i=0; i<CSW_NUM_FUNC; i++) b->addItem(getCSWFunc(i));
-    b->setCurrentIndex(value);
-    b->setMaxVisibleItems(10);
+void populateCSWCB(QComboBox *b, int value) {
+  b->clear();
+  for (int i = 0; i < CSW_NUM_FUNC; i++) b->addItem(getCSWFunc(i));
+  b->setCurrentIndex(value);
+  b->setMaxVisibleItems(10);
 }
 
-QString getSignedStr(int value)
-{
+QString getSignedStr(int value) {
   return value > 0 ? QString("+%1").arg(value) : QString("%1").arg(value);
 }
 
-QString getCurveStr(int curve)
-{
+QString getCurveStr(int curve) {
   QString crvStr = "---x>0x<0|x|f>0f<0|f|c1 c2 c3 c4 c5 c6 c7 c8 c9 c10c11c12c13c14c15c16";
-  return crvStr.mid(curve*3, 3).remove(' ').replace("c", QObject::tr("Curve") + " ");
+  return crvStr.mid(curve * 3, 3).remove(' ').replace("c", QObject::tr("Curve") + " ");
 }
 
+QString image2qstring(QImage image) {
+  image.scaled(128, 64);
+  uchar b[1024] = {0};
+  quint8 * p = image.bits();
+  for (int y = 0; y < 64; y++) {
+    for (int x = 0; x < 16; x++) {
+      for (int k = 0; k < 8; k++) {
+        b[y * 16 + x] |= (((p[(y * 128 + x * 8 + k) / 8] & (1 << k)) ? 1 : 0) << (7 - k));
+      }
+    }
+  }
+  QString ImageStr;
+  ImageStr.clear();
+  for (int j = 0; j < 128 * 8; j++)
+    ImageStr += QString("%1").arg(b[ j]&0xff, 2, 16, QChar('0'));
+  return ImageStr;
+}
 
+QImage qstring2image(QString imagestr) {
+  uchar b[1024] = {0};
+  bool ok;
+  bool failed = false;
+  int pixel;
+  QImage Image(128, 64, QImage::Format_MonoLSB);
+  Image.setColor(0, qRgb(255, 255, 255));
+  Image.setColor(1, qRgb(0, 0, 0));
+  int len = imagestr.length();
+  if (len == 2048) {
+    for (int i = 0; i < 1024; i++) {
+      QString Byte;
+      Byte = imagestr.mid((i * 2), 2);
+      b[i] = Byte.toUInt(&ok, 16);
+      if (!ok) {
+        failed = true;
+      }
+    }
+    if (!failed) {
+      for (int y = 0; y < 64; y++)
+        for (int x = 0; x < 16; x++)
+          for (int k = 0; k < 8; k++) {
+            pixel = ((b[y * 16 + x]&(1 << k)) ? 1 : 0);
+            Image.setPixel((x * 8)+(7 - k), y, pixel);
+          }
+    }
+  }
+  return Image;
+}
--- src/preferencesdialog.cpp
+++ src/preferencesdialog.cpp
@@ -2,120 +2,164 @@
 #include "ui_preferencesdialog.h"
 #include "mainwindow.h"
 #include "eeprominterface.h"
+#include "helpers.h"
 #include <QtGui>
 
 preferencesDialog::preferencesDialog(QWidget *parent) :
-    QDialog(parent),
-    ui(new Ui::preferencesDialog)
-{
-    ui->setupUi(this);
+QDialog(parent),
+ui(new Ui::preferencesDialog) {
+  ui->setupUi(this);
 
-    populateLocale();
-    initSettings();
-    connect(ui->downloadVerCB, SIGNAL(currentIndexChanged(int)), this, SLOT(firmwareChanged()));
-    connect(this, SIGNAL(accepted()), this, SLOT(writeValues()));
+  populateLocale();
+  initSettings();
+  connect(ui->downloadVerCB, SIGNAL(currentIndexChanged(int)), this, SLOT(firmwareChanged()));
+  connect(this, SIGNAL(accepted()), this, SLOT(writeValues()));
 }
 
-preferencesDialog::~preferencesDialog()
-{
-    delete ui;
+preferencesDialog::~preferencesDialog() {
+  delete ui;
 }
 
-void preferencesDialog::firmwareChanged()
-{
-    QString fwid;
-    QVariant selected_firmware = ui->downloadVerCB->itemData(ui->downloadVerCB->currentIndex());
-    foreach(FirmwareInfo firmware, firmwares) {
-        if (firmware.id == selected_firmware) {
-          fwid=firmware.id;
-          ui->fw_dnld->setEnabled(firmware.url);
-          break;
-        }
+void preferencesDialog::firmwareChanged() {
+  QString fwid;
+  QVariant selected_firmware = ui->downloadVerCB->itemData(ui->downloadVerCB->currentIndex());
+
+  foreach(FirmwareInfo firmware, firmwares) {
+    if (firmware.id == selected_firmware) {
+      fwid = firmware.id;
+      ui->fw_dnld->setEnabled(firmware.url);
+      break;
     }
-    QSettings settings("companion9x", "companion9x");
-    settings.beginGroup("FwRevisions");
-    int fwrev=settings.value(fwid, -1).toInt();
-    settings.endGroup();
-    if (fwrev!=-1) {
-        ui->FwInfo->setText(tr("Last downloaded release: %1").arg(fwrev));
-    } else {
-        if  (ui->fw_dnld->isEnabled()) {
-            ui->FwInfo->setText(tr("The selected firmware has never been downloaded by companion9x."));
-        } else {
-            ui->FwInfo->setText(tr("The selected firmware cannot be downloaded by companion9x."));
-        }
+  }
+  QSettings settings("companion9x", "companion9x");
+  settings.beginGroup("FwRevisions");
+  int fwrev = settings.value(fwid, -1).toInt();
+  settings.endGroup();
+  if (fwrev != -1) {
+    ui->FwInfo->setText(tr("Last downloaded release: %1").arg(fwrev));
+  }
+  else {
+    if (ui->fw_dnld->isEnabled()) {
+      ui->FwInfo->setText(tr("The selected firmware has never been downloaded by companion9x."));
     }
+    else {
+      ui->FwInfo->setText(tr("The selected firmware cannot be downloaded by companion9x."));
+    }
+  }
 }
 
-void preferencesDialog::writeValues()
-{
-    QSettings settings("companion9x", "companion9x");
-    if (ui->locale_QB->currentIndex() > 0)
-      settings.setValue("locale", ui->locale_QB->itemData(ui->locale_QB->currentIndex()));
-    else
-      settings.remove("locale");
-    settings.setValue("default_channel_order", ui->channelorderCB->currentIndex());
-    settings.setValue("default_mode", ui->stickmodeCB->currentIndex());
-    settings.setValue("startup_check_companion9x", ui->startupCheck_companion9x->isChecked());
-    settings.setValue("startup_check_fw", ui->startupCheck_fw->isChecked());
-    settings.setValue("show_splash", ui->showSplash->isChecked());
-    settings.setValue("history_size", ui->historySize->value());
-    settings.setValue("firmware", ui->downloadVerCB->itemData(ui->downloadVerCB->currentIndex()));
-    settings.setValue("backLight", ui->backLightColor->currentIndex());
+void preferencesDialog::writeValues() {
+  QSettings settings("companion9x", "companion9x");
+  if (ui->locale_QB->currentIndex() > 0)
+    settings.setValue("locale", ui->locale_QB->itemData(ui->locale_QB->currentIndex()));
+  else
+    settings.remove("locale");
+  settings.setValue("default_channel_order", ui->channelorderCB->currentIndex());
+  settings.setValue("default_mode", ui->stickmodeCB->currentIndex());
+  settings.setValue("startup_check_companion9x", ui->startupCheck_companion9x->isChecked());
+  settings.setValue("startup_check_fw", ui->startupCheck_fw->isChecked());
+  settings.setValue("show_splash", ui->showSplash->isChecked());
+  settings.setValue("history_size", ui->historySize->value());
+  settings.setValue("firmware", ui->downloadVerCB->itemData(ui->downloadVerCB->currentIndex()));
+  settings.setValue("backLight", ui->backLightColor->currentIndex());
+  if (!ui->SplashFileName->text().isEmpty()) {
+    QImage Image = ui->imageLabel->pixmap()->toImage().convertToFormat(QImage::Format_MonoLSB);
+    settings.setValue("SplashImage", image2qstring(Image));
+    settings.setValue("SplashFileName", ui->SplashFileName->text());
+    settings.setValue("ApplySplashToFw", ui->AutoApplySplash->isChecked());
+  }
 }
 
-void preferencesDialog::initSettings()
-{
-    QSettings settings("companion9x", "companion9x");
-    int i=ui->locale_QB->findData(settings.value("locale"));
-    if(i<0) i=0;
-    ui->locale_QB->setCurrentIndex(i);
+void preferencesDialog::initSettings() {
+  QSettings settings("companion9x", "companion9x");
+  int i = ui->locale_QB->findData(settings.value("locale"));
+  if (i < 0) i = 0;
+  ui->locale_QB->setCurrentIndex(i);
 
-    ui->channelorderCB->setCurrentIndex(settings.value("default_channel_order", 0).toInt());
-    ui->stickmodeCB->setCurrentIndex(settings.value("default_mode", 1).toInt());
-    ui->startupCheck_companion9x->setChecked(settings.value("startup_check_companion9x", true).toBool());
-    ui->showSplash->setChecked(settings.value("show_splash", true).toBool());
-    ui->historySize->setValue(settings.value("history_size", 10).toInt());
-    ui->backLightColor->setCurrentIndex(settings.value("backLight", 0).toInt());
-    ui->startupCheck_fw->setChecked(settings.value("startup_check_fw", true).toBool());
-    FirmwareInfo current_firmware = GetCurrentFirmware();
-    foreach(FirmwareInfo firmware, firmwares) {
-      ui->downloadVerCB->addItem(firmware.name, firmware.id);
-      if (firmware.id == current_firmware.id)
-        ui->downloadVerCB->setCurrentIndex(ui->downloadVerCB->count() - 1);
-    }
+  ui->channelorderCB->setCurrentIndex(settings.value("default_channel_order", 0).toInt());
+  ui->stickmodeCB->setCurrentIndex(settings.value("default_mode", 1).toInt());
+  ui->startupCheck_companion9x->setChecked(settings.value("startup_check_companion9x", true).toBool());
+  ui->showSplash->setChecked(settings.value("show_splash", true).toBool());
+  ui->historySize->setValue(settings.value("history_size", 10).toInt());
+  ui->backLightColor->setCurrentIndex(settings.value("backLight", 0).toInt());
+  ui->startupCheck_fw->setChecked(settings.value("startup_check_fw", true).toBool());
+  FirmwareInfo current_firmware = GetCurrentFirmware();
 
-    firmwareChanged();
+  foreach(FirmwareInfo firmware, firmwares) {
+    ui->downloadVerCB->addItem(firmware.name, firmware.id);
+    if (firmware.id == current_firmware.id)
+      ui->downloadVerCB->setCurrentIndex(ui->downloadVerCB->count() - 1);
+  }
+  QString ImageStr = settings.value("SplashImage", "").toString();
+  if (!ImageStr.isEmpty()) {
+    ui->AutoApplySplash->setEnabled(true);
+    ui->AutoApplySplash->setChecked(settings.value("ApplySplashToFw", false).toBool());
+    QImage Image = qstring2image(ImageStr);
+    ui->imageLabel->setPixmap(QPixmap::fromImage(Image.convertToFormat(QImage::Format_Mono)));
+    ui->InvertPixels->setEnabled(true);
+    ui->SplashFileName->setText(tr("Image stored in settings"));
+  }
+  firmwareChanged();
 }
 
-void preferencesDialog::populateLocale()
-{
-    ui->locale_QB->clear();
-    ui->locale_QB->addItem("System default language", "");
-    ui->locale_QB->addItem("English", "en");
+void preferencesDialog::populateLocale() {
+  ui->locale_QB->clear();
+  ui->locale_QB->addItem("System default language", "");
+  ui->locale_QB->addItem("English", "en");
 
-    QStringList strl = QApplication::arguments();
-    if(!strl.count()) return;
+  QStringList strl = QApplication::arguments();
+  if (!strl.count()) return;
 
-    QDir directory = QDir(":/");
-    QStringList files = directory.entryList(QStringList("companion9x_*.qm"), QDir::Files | QDir::NoSymLinks);
+  QDir directory = QDir(":/");
+  QStringList files = directory.entryList(QStringList("companion9x_*.qm"), QDir::Files | QDir::NoSymLinks);
 
-    foreach(QString file, files) {
-      QLocale loc(file.mid(12,2));
-      ui->locale_QB->addItem(QLocale::languageToString(loc.language()), loc.name());
-    }
+  foreach(QString file, files) {
+    QLocale loc(file.mid(12, 2));
+    ui->locale_QB->addItem(QLocale::languageToString(loc.language()), loc.name());
+  }
 }
 
-void preferencesDialog::on_fw_dnld_clicked()
-{
-    MainWindow * mw = (MainWindow *)this->parent();
-    QString firmware_id = ui->downloadVerCB->itemData(ui->downloadVerCB->currentIndex()).toString();
-    foreach(FirmwareInfo firmware, firmwares) {
-      if (firmware.id == firmware_id) {
-        mw->downloadLatestFW(&firmware);
-        break;
-      }
-    }
+void preferencesDialog::on_fw_dnld_clicked() {
+  MainWindow * mw = (MainWindow *)this->parent();
+  QString firmware_id = ui->downloadVerCB->itemData(ui->downloadVerCB->currentIndex()).toString();
 
-    firmwareChanged();
+  foreach(FirmwareInfo firmware, firmwares) {
+    if (firmware.id == firmware_id) {
+      mw->downloadLatestFW(&firmware);
+      break;
+    }
+  }
+
+  firmwareChanged();
+}
+
+void preferencesDialog::on_SplashSelect_clicked() {
+  QString supportedImageFormats;
+  for (int formatIndex = 0; formatIndex < QImageReader::supportedImageFormats().count(); formatIndex++) {
+    supportedImageFormats += QLatin1String(" *.") + QImageReader::supportedImageFormats()[formatIndex];
+  }
+
+  QSettings settings("companion9x", "companion9x");
+  QString fileName = QFileDialog::getOpenFileName(this,
+          tr("Open Image to load"), settings.value("lastDir").toString(), tr("Images (%1)").arg(supportedImageFormats));
+
+  if (!fileName.isEmpty()) {
+    QImage image(fileName);
+    if (image.isNull()) {
+      QMessageBox::critical(this, tr("Error"), tr("Cannot load %1.").arg(fileName));
+      return;
+    }
+    ui->SplashFileName->setText(fileName);
+    ui->imageLabel->setPixmap(QPixmap::fromImage(image.scaled(128, 64).convertToFormat(QImage::Format_Mono)));
+    ui->AutoApplySplash->setEnabled(true);
+    ui->InvertPixels->setEnabled(true);
+    //    ui->SaveFlashButton->setEnabled(true);
+    //    ui->HowToLabel->setText(tr("Save your custimized firmware"));
+  }
+}
+
+void preferencesDialog::on_InvertPixels_clicked() {
+  QImage image = ui->imageLabel->pixmap()->toImage();
+  image.invertPixels();
+  ui->imageLabel->setPixmap(QPixmap::fromImage(image));
 }
