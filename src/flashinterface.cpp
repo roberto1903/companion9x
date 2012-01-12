973495da282db800ec5c2e0b926036542ab87014
romolo.manfredini <romolo.manfredini@gmail.com>
2012-01-12 12:23:51 +0000

    Compatibility fix for newer er9x splash marker enabled files.

--- src/flashinterface.cpp
+++ src/flashinterface.cpp
@@ -244,7 +244,8 @@ void FlashInterface::SeekSplash(void)
   }
   if (start==-1) {
     splash.clear();
-    splash.append(SPLASH_MARK,sizeof(SPLASH_MARK));
+    splash.append(SPLASH_MARK);
+    splash.append('\0');
     start = flash.indexOf(splash);
     if (start>0) {
       splash_offset=start+10;
