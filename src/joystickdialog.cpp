7ad9c7673a9942d324aa301bd8e9e3d15a196c02
romolo.manfredini <romolo.manfredini@gmail.com>
2012-01-20 00:45:10 +0000

    Initial joystick interface support (continued)

--- src/joystickdialog.cpp
+++ src/joystickdialog.cpp
@@ -1,84 +1,100 @@
 #include "joystickdialog.h"
 #include "ui_joystickdialog.h"
+#include <QtGui>
 
-joystickDialog::joystickDialog(QWidget *parent) :
-    QDialog(parent),
-    ui(new Ui::joystickDialog)
-{
-    ui->setupUi(this);
+joystickDialog::joystickDialog(QWidget *parent, int stick) :
+QDialog(parent),
+ui(new Ui::joystickDialog) {
+  ui->setupUi(this);
+  int jscaltmp[8][3] = {
+    {32767, 0, -32767},
+    {32767, 0, -32767},
+    {32767, 0, -32767},
+    {32767, 0, -32767},
+    {32767, 0, -32767},
+    {32767, 0, -32767},
+    {32767, 0, -32767},
+    {32767, 0, -32767}
+  };
+  memcpy(jscal, jscaltmp, sizeof (jscal));
+  ui->okButton->setDisabled(true);
+  ui->howtoLabel->setText(tr("Move sticks and pots in every direction making full movement\nPress next when finished"));
+  step = 0;
+
+  foreach(QComboBox *cb, findChildren<QComboBox *>(QRegExp("jsmapCB_[0-9]+"))) {
+    cb->setDisabled(true);
+  }
+  joystickOpen(stick);
 }
 
-joystickDialog::~joystickDialog()
-{
-    delete ui;
+joystickDialog::~joystickDialog() {
+  delete ui;
 }
 
-void joystickDialog::joystickScan() {
-    QStringList joystickNames;
-    joystickNames << tr("No joysticks found");
-    joystick = new Joystick(0,true,0,0);
+void joystickDialog::joystickOpen(int stick) {
+  if (stick == -1) {
+    this->close();
+    return;
+  }
+  joystick = new Joystick(this, 1, false, 0);
 
-    if ( joystick )
-      if ( joystick->joystickNames.count() > 0 )
-        joystickNames = joystick->joystickNames;
-
-    ui->joystickCB->clear();
-    ui->joystickCB->insertItems(0, joystickNames);
-    if ( joystick->joystickNames.count() > 1 ) {
-      ui->joystickCB->setEnabled(true);
-    } else if ( joystick->joystickNames.count() == 1 ) {
-      joystick->open(0);
-      ui->Ch_1->setMinimum(0);
-      ui->Ch_1->setMaximum(0);
-      connect(joystick, SIGNAL(axisValueChanged(int, int)), this, SLOT(on_joystickAxisValueChanged(int, int)));
-      ui->joystickCB->setDisabled(true);
+  if (joystick) {
+    joystick->open(stick);
+    for (int j=0; j<joystick->numAxes; j++) {
+        joystick->sensitivities[j] = 0;
+        joystick->deadzones[j]=20;
     }
-    else {
-        ui->joystickCB->setDisabled(true);
-    }
-}
-
-void joystickDialog::on_joystickChkB_clicked() {
-  if (ui->joystickChkB->isChecked()) {
-    ui->joystickTB->setEnabled(true);
-    joystickScan();
+    connect(joystick, SIGNAL(axisValueChanged(int, int)), this, SLOT(on_joystickAxisValueChanged(int, int)));
   }
   else {
-    ui->joystickCB->clear();
-    ui->joystickCB->setDisabled(true);
+    QMessageBox::critical(this, tr("Error"), tr("Cannot open joystick."));
+    this->close();
   }
-}
-
-void joystickDialog::on_joystickTB_clicked() {
-  joystickScan();
 }
 
 void joystickDialog::on_joystickAxisValueChanged(int axis, int value) {
-  switch (axis) {
+  if (value > jscal[axis][2]) {
+    jscal[axis][2] = value;
+  }
+  if (value < jscal[axis][0]) {
+    jscal[axis][0] = value;
+  }
+  QSlider * sl[]= {ui->Ch_1, ui->Ch_2, ui->Ch_3, ui->Ch_4, ui->Ch_5, ui->Ch_6, ui->Ch_7, ui->Ch_8};
+  sl[axis]->setMinimum(jscal[axis][0]);
+  sl[axis]->setMaximum(jscal[axis][2]);
+  sl[axis]->setValue(value);
+}
+
+void joystickDialog::on_nextButton_clicked() {
+  switch (step) {
     case 0:
-      if (value>ui->Ch_1->maximum()) {
-        ui->Ch_1->setMaximum(value);
-      }
-      if (value<ui->Ch_1->minimum()) {
-        ui->Ch_1->setMinimum(value);
-      }
-      ui->Ch_1->setValue(value);
+      ui->howtoLabel->setText(tr("Place sticks and pots in middle position.\nPress next when done"));
+      step++;
       break;
     case 1:
-      ui->Ch_2->setValue(value);
+      for (int i=0; i< joystick->numAxes; i++) {
+        jscal[i][1]=0;
+        for (int j=0; j<100;j++) {
+          jscal[i][1]+=joystick->getAxisValue(i);
+        }
+        jscal[i][1]/=100;
+      }
+      ui->howtoLabel->setText(tr("Map joystick channels to controls using comboboxes.\nPress next when done"));
+      step++;
+      foreach(QComboBox *cb, findChildren<QComboBox *>(QRegExp("jsmapCB_[0-9]+"))) {
+        cb->setEnabled(true);
+      }
       break;
     case 2:
-      ui->Ch_3->setValue(value);
-      break;
-    case 3:
-      ui->Ch_4->setValue(value);
-      break;
-    case 4:
-      ui->Ch_5->setValue(value);
-      break;
-    case 5:
-      ui->Ch_6->setValue(value);
-      break;
-
+      ui->howtoLabel->setText(tr("Press ok to save configuration\nPress cancel to abort joystick calibration"));
+      ui->okButton->setEnabled(true);
   }
+}
+
+void joystickDialog::on_cancelButton_clicked() {
+  this->close();
+}
+
+void joystickDialog::on_okButton_clicked() {
+  this->close();
 }
--- src/joystickdialog.h
+++ src/joystickdialog.h
@@ -13,19 +13,24 @@ class joystickDialog : public QDialog
     Q_OBJECT
 
 public:
-    explicit joystickDialog(QWidget *parent = 0);
+    explicit joystickDialog(QWidget *parent = 0, int stick=-1);
     ~joystickDialog();
     Joystick *joystick;
 
+public slots:
+    void on_joystickAxisValueChanged(int axis, int value);
+    
 private:
     Ui::joystickDialog *ui;
-    void joystickScan();
+    void joystickOpen(int stick);
+    int jscal[8][3];
+    int step;
     
-
 private slots:
-    void on_joystickChkB_clicked();
-    void on_joystickTB_clicked();
-    void on_joystickAxisValueChanged(int axis, int value);
+    void on_nextButton_clicked();
+    void on_cancelButton_clicked();
+    void on_okButton_clicked();
+    
 };
 
 #endif // JOYSTICKDIALOG_H
