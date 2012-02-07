fa581bb3cb683f5df54e59cafc2b66784e036ea6
romolo.manfredini@gmail.com <romolo.manfredini@gmail.com@6b3858b5-4ca4-83a9-f672-1595cfb9b461>
2012-02-07 11:45:24 +0000

    Fix for function switch tab

--- trunk/src/modeledit.h
+++ trunk/src/modeledit.h
@@ -50,7 +50,8 @@ private:
 
     QComboBox * fswtchSwtch[NUM_FSW];
     QComboBox * fswtchFunc[NUM_FSW];
-
+    QSpinBox * fswtchParam[NUM_FSW];
+    
     QSpinBox  * safetySwitchValue[NUM_CHNOUT];
     QComboBox * safetySwitchSwtch[NUM_CHNOUT];
 
