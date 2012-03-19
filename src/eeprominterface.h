bcc1cae2b10a3b3a2064ec163ed909856c7e70f7
romolo.manfredini <romolo.manfredini@gmail.com>
2012-03-19 23:09:59 +0000

    Update for telemetry values in custom switches

--- src/eeprominterface.h
+++ src/eeprominterface.h
@@ -193,7 +193,7 @@ enum EnumKeys {
 
 #define MAX_TIMERS      2
 
-#define NUM_TELEMETRY   9
+#define NUM_TELEMETRY   12
 #define TELEMETRY_CHANNELS  "AD1 AD2 "
 #define TM_HASTELEMETRY     0x01
 #define TM_HASOFFSET        0x02
