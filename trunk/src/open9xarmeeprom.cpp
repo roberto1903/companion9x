e7e3899d8ac21f4c6e4aacff28fb86a94967b339
romolo.manfredini@gmail.com <romolo.manfredini@gmail.com@6b3858b5-4ca4-83a9-f672-1595cfb9b461>
2012-05-19 20:44:49 +0000

    switchWarningStates eeprom values exported to open9x

--- trunk/src/open9xarmeeprom.cpp
+++ trunk/src/open9xarmeeprom.cpp
@@ -935,6 +935,7 @@ t_Open9xArmModelData_v209::operator ModelData ()
   c9x.frsky.varioSource = varioSource;
   c9x.frsky.varioSpeedUpMin = varioSpeedUpMin;
   c9x.frsky.varioSpeedDownMin = varioSpeedDownMin;
+  c9x.switchWarningStates = switchWarningStates;
   c9x.ppmFrameLength = ppmFrameLength;
   c9x.thrTraceSrc = thrTraceSrc;
   c9x.modelId = modelId;
@@ -1040,7 +1041,8 @@ t_Open9xArmModelData_v209::t_Open9xArmModelData_v209(ModelData &c9x)
     varioSource = c9x.frsky.varioSource;
     varioSpeedUpMin = c9x.frsky.varioSpeedUpMin;
     varioSpeedDownMin = c9x.frsky.varioSpeedDownMin;
-
+    switchWarningStates = c9x.switchWarningStates;
+    
     ppmFrameLength = c9x.ppmFrameLength;
     thrTraceSrc = c9x.thrTraceSrc;
     modelId = c9x.modelId;
