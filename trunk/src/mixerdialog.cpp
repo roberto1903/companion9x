8e5c2203c4e8b1cf1df92cfb4f2610a86194e450
romolo.manfredini@gmail.com <romolo.manfredini@gmail.com@6b3858b5-4ca4-83a9-f672-1595cfb9b461>
2012-07-27 18:48:10 +0000

    Support for new curves in mixers

--- trunk/src/mixerdialog.cpp
+++ trunk/src/mixerdialog.cpp
@@ -110,7 +110,11 @@ void MixerDialog::valuesChanged()
     md->sOffset   = ui->offsetSB->value();
     md->carryTrim = -(ui->trimCB->currentIndex()-1);
     md->enableFmTrim = ui->FMtrimChkB->checkState() ? 1 : 0;
-// TODO here we need to introduce a new capacity!!!    md->curve     = ui->curvesCB->currentIndex()-(MAX_CURVES)*GetEepromInterface()->getCapability(HasNegCurves);
+    int numcurves=GetEepromInterface()->getCapability(NumCurves);
+    if (numcurves==0) {
+      numcurves=16;
+    }
+    md->curve     = ui->curvesCB->currentIndex()-(numcurves)*GetEepromInterface()->getCapability(HasNegCurves);
     md->phase     = ui->phasesCB->itemData(ui->phasesCB->currentIndex()).toInt();
     md->swtch     = RawSwitch(ui->switchesCB->itemData(ui->switchesCB->currentIndex()).toInt());
     md->mixWarn   = ui->warningCB->currentIndex();
