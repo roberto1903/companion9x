df4ba6f2076757358b4d4f8a9ac83ed3265093e0
romolo.manfredini@gmail.com <romolo.manfredini@gmail.com@6b3858b5-4ca4-83a9-f672-1595cfb9b461>
2012-07-29 22:33:44 +0000

    Issue 66 fix

--- trunk/src/open9xarmeeprom.cpp
+++ trunk/src/open9xarmeeprom.cpp
@@ -870,8 +870,8 @@ t_Open9xArmFuncSwData_v211::t_Open9xArmFuncSwData_v211(FuncSwData &c9x)
   swtch = open9xArmFromSwitch(c9x.swtch);
   uint32_t value;
   if (c9x.func <= FuncSafetyCh16) {
-    value = ((c9x.param>>1)<<1);
-    value |=(c9x.enabled & 0x01);
+    value = (c9x.param);
+    delay=(c9x.enabled & 0x01);
   }
   else {
     if (c9x.func != FuncPlayPrompt) {
@@ -891,8 +891,8 @@ t_Open9xArmFuncSwData_v211::operator FuncSwData ()
   c9x.func = (AssignFunc)(func);
   uint32_t value = *((uint32_t *)param);
   if (c9x.func <= FuncSafetyCh16) {
-    c9x.enabled = value & 0x01;
-    c9x.param = (value>>1)<<1;
+    c9x.enabled = delay & 0x01;
+    c9x.param = value;
   }
   else {
     if (c9x.func != FuncPlayPrompt) {
