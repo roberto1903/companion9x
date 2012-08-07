789deb0b7ba17afb18c230afdf4e89a3ed24f555
romolo.manfredini@gmail.com <romolo.manfredini@gmail.com@6b3858b5-4ca4-83a9-f672-1595cfb9b461>
2012-08-07 14:43:40 +0000

    Fix for issue 71

--- trunk/src/modeledit.cpp
+++ trunk/src/modeledit.cpp
@@ -1485,7 +1485,7 @@ void ModelEdit::tabFunctionSwitches()
       fswtchSwtch[i] = new QComboBox(this);
       connect(fswtchSwtch[i],SIGNAL(currentIndexChanged(int)),this,SLOT(functionSwitchesEdited()));
       ui->fswitchlayout2->addWidget(fswtchSwtch[i],i-15,1);
-      populateSwitchCB(fswtchSwtch[i], g_model.funcSw[i].swtch);
+      populateSwitchCB(fswtchSwtch[i], g_model.funcSw[i].swtch, POPULATE_MSWITCHES|POPULATE_ONOFF);
 
       fswtchFunc[i] = new QComboBox(this);
       connect(fswtchFunc[i],SIGNAL(currentIndexChanged(int)),this,SLOT(functionSwitchesEdited()));
