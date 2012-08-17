c731b6d9d3cf2c35ccc5fdb1a68f484755236dc2
romolo.manfredini@gmail.com <romolo.manfredini@gmail.com@6b3858b5-4ca4-83a9-f672-1595cfb9b461>
2012-08-17 10:31:57 +0000

    Store phases in Mixers, reorder of telemetry inputs.

--- trunk/src/warnings.h
+++ trunk/src/warnings.h
@@ -1,8 +1,8 @@
 #ifndef WARNINGS_H
 #define	WARNINGS_H
 
-#define WARNING_ID 2
-#define WARNING QObject::tr("Warning!\nHandling of slow up an down in open9x mix has been changed, please check your settings!")
+#define WARNING_ID 3
+#define WARNING QObject::tr("Warning!\nTelemetry channel order has been changed, please check function switches and telemetry settings!")
 
 #endif	/* WARNINGS_H */
 
