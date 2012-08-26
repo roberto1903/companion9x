a28c4ca829e2289a412fe9195d6749724bdb7c19
romolo.manfredini@gmail.com <romolo.manfredini@gmail.com@6b3858b5-4ca4-83a9-f672-1595cfb9b461>
2012-08-26 20:42:04 +0000

    Bug in tooltips for preferences

--- trunk/src/preferencesdialog.cpp
+++ trunk/src/preferencesdialog.cpp
@@ -357,7 +357,7 @@ const char * options[]={
         "ppmca",
         "potscroll",
         "autoswitch",
-        "dblkeys"
+        "dblkeys",
         "pgbar",
         "imperial",
         "speaker",
