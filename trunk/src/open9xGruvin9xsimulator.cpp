715c0f93630ba94b29791b0b023b82b2ed6843a6
bsongis <bsongis@6b3858b5-4ca4-83a9-f672-1595cfb9b461>
2013-01-31 21:22:29 +0000

    Trims problem in open9x simulators

--- trunk/src/open9xGruvin9xsimulator.cpp
+++ trunk/src/open9xGruvin9xsimulator.cpp
@@ -150,7 +150,7 @@ void Open9xGruvin9xSimulator::setValues(TxInputs &inputs)
 
 void Open9xGruvin9xSimulator::setTrim(unsigned int idx, int value)
 {
-  idx = Open9xGruvin9x::modn12x3[4*getStickMode()+idx-1];
+  idx = Open9xGruvin9x::modn12x3[4*getStickMode() + idx] - 1;
   uint8_t phase = getTrimFlightPhase(getFlightPhase(), idx);
   setTrimValue(phase, idx, value);
 }
@@ -164,7 +164,7 @@ void Open9xGruvin9xSimulator::getTrims(Trims & trims)
   }
 
   for (int i=0; i<2; i++) {
-    uint8_t idx = Open9xGruvin9x::modn12x3[4*getStickMode()+i-1];
+    uint8_t idx = Open9xGruvin9x::modn12x3[4*getStickMode() + i] - 1;
     int16_t tmp = trims.values[i];
     trims.values[i] = trims.values[idx];
     trims.values[idx] = tmp;
--- trunk/src/open9xM128simulator.cpp
+++ trunk/src/open9xM128simulator.cpp
@@ -147,7 +147,7 @@ void Open9xM128Simulator::setValues(TxInputs &inputs)
 
 void Open9xM128Simulator::setTrim(unsigned int idx, int value)
 {
-  idx = Open9xM128::modn12x3[4*getStickMode()+idx-1];
+  idx = Open9xM128::modn12x3[4*getStickMode() + idx] - 1;
   uint8_t phase = getTrimFlightPhase(getFlightPhase(), idx);
   setTrimValue(phase, idx, value);
 }
@@ -161,7 +161,7 @@ void Open9xM128Simulator::getTrims(Trims & trims)
   }
 
   for (int i=0; i<2; i++) {
-    uint8_t idx = Open9xM128::modn12x3[4*getStickMode()+i-1];
+    uint8_t idx = Open9xM128::modn12x3[4*getStickMode() + i] - 1;
     int16_t tmp = trims.values[i];
     trims.values[i] = trims.values[idx];
     trims.values[idx] = tmp;
