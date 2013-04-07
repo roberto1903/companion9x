528b9d0e5746f0f6a63b6fce4a85274a756e42c2
bsongis <bsongis@6b3858b5-4ca4-83a9-f672-1595cfb9b461>
2013-04-07 13:58:36 +0000

    256 shift in PlayTrack function (stock / gruvin9x)

--- src/modeledit.cpp
+++ src/modeledit.cpp
@@ -2175,10 +2175,9 @@ void ModelEdit::refreshCustomFunction(int i, bool modified)
     else if (index==FuncPlayPrompt || index==FuncPlayBoth) {
       if (GetEepromInterface()->getCapability(VoicesAsNumbers)) {
         widgetsMask |= CUSTOM_FUNCTION_NUMERIC_PARAM;
-        fswtchParam[i]->setMinimum(256);
-        fswtchParam[i]->setMaximum(index==FuncPlayBoth ? 510 : 511);
-        if (modified) g_model.funcSw[i].param = fswtchParam[i]->value()-256;
-        fswtchParam[i]->setValue(256+g_model.funcSw[i].param);
+        fswtchParam[i]->setMaximum(index==FuncPlayBoth ? 254 : 255);
+        if (modified) g_model.funcSw[i].param = fswtchParam[i]->value();
+        fswtchParam[i]->setValue(g_model.funcSw[i].param);
 #ifdef PHONON
         playBT[i]->show();
 #endif
