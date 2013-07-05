afab0d25f7c6d8107a469e447e6927f116a22b08
romolo.manfredini@gmail.com <romolo.manfredini@gmail.com@6b3858b5-4ca4-83a9-f672-1595cfb9b461>
2013-07-05 05:21:52 +0000

    SD PATH (remove debug info)

--- trunk/src/opentxTaranisSimulator.cpp
+++ trunk/src/opentxTaranisSimulator.cpp
@@ -146,7 +146,6 @@ OpentxTaranisSimulator::OpentxTaranisSimulator(Open9xInterface * open9xInterface
   taranisSimulatorBoard = GetEepromInterface()->getBoard();
   QSettings settings("companion9x", "companion9x");
   QString path=settings.value("sdPath", "").toString()+"/";
-  qDebug() << path;
   int i=0;
   for (i=0; i< std::min(path.length(),1022); i++) {
     simuSdDirectory[i]=path.at(i).toAscii();
