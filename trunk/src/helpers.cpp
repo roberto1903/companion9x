b14dbd6eae55b2e87a32bb4f2fde56eff3b44015
romolo.manfredini@gmail.com <romolo.manfredini@gmail.com@6b3858b5-4ca4-83a9-f672-1595cfb9b461>
2013-07-05 22:18:44 +0000

    SD PATH defaults to a better value

--- trunk/src/helpers.cpp
+++ trunk/src/helpers.cpp
@@ -276,7 +276,7 @@ QString getProtocolStr(const int proto)
                                    "CTP1009",
                                    "LP45", "DSM2", "DSMX",
                                    "PPM16", "PPMsim",
-                                   "FrSky XJT - X16", "FrSky XJT - D8", "FrSky XJT - LR12", "FrSky DJT",
+                                   "FrSky XJT - D16", "FrSky XJT - D8", "FrSky XJT - LR12", "FrSky DJT",
   };
 
   return CHECK_IN_ARRAY(strings, proto);
