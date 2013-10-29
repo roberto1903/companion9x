6198a6362f98aa8e6512b252b7c513e8275b79a6
bsongis <bsongis@6b3858b5-4ca4-83a9-f672-1595cfb9b461>
2013-10-29 16:53:00 +0000

    Line ends in the warning message.

--- trunk/src/warnings.h
+++ trunk/src/warnings.h
@@ -2,7 +2,7 @@
 #define	WARNINGS_H
 #define WARNING_LEVEL 0
 #define WARNING_ID 6
-#define WARNING QObject::tr("Thanks all for the donations, kind messages, nice ideas, constructive\nproposals etc. for the next openTX/c9x funding model.\nWe are really sensible to that!\nThe openTX/C9X Team.")
+#define WARNING QObject::tr("Thanks all for the donations, kind messages, nice ideas, constructive proposals etc. for the next openTX/C9x funding model.\n\nWe are really sensible to that!\n\nThe openTX/C9X Team.")
 
 #endif	/* WARNINGS_H */
 
