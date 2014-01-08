e549c89b3b984aa5e0a98522100a24c27f3d5244
bsongis <bsongis@6b3858b5-4ca4-83a9-f672-1595cfb9b461>
2014-01-08 10:08:11 +0000

    Update to latest OpenTX SVN

--- trunk/src/opentxTaranisSimulator.cpp
+++ trunk/src/opentxTaranisSimulator.cpp
@@ -111,6 +111,7 @@ inline int geteepromsize() {
 #include "../opentx/translations/tts_fr.cpp"
 #include "../opentx/translations/tts_pt.cpp"
 #include "../opentx/translations/tts_sk.cpp"
+#include "../opentx/translations/tts_pl.cpp"
   
 int16_t g_anas[NUM_STICKS+BOARD_X9D_NUM_POTS];
 
